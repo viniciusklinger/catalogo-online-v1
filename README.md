c34b57d4672a52f0cbb1380b9d31d74e8dc5e73d
Vinicius Klinger <viniciusklinger@yahoo.com.br>
2023-09-12 00:53:01 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# catalogo-online-v1
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      screens: {
+        'xs': '475px',
+      ...defaultTheme.screens,
+      },
+      colors: {
+        primary: '#ffbf00',
+        default: '#212121',
+      },
+      fontFamily: {
+        pop: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/line-clamp'),
+  ],
+}
+
